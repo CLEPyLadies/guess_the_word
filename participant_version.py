fe30b32b8f98706f11e28171bc750d6818adfac4
mutterberg <Marissa@UtterbergDataDev.com>
2021-08-14 13:32:01 -0400

    WIP

--- participant_version.py
+++ participant_version.py
@@ -3,38 +3,16 @@
    
 import random
 
-
-
 #Next we will declare the variable that holds the number of guesses or #chances that our player has to correctly guess the word
 
 #put you code in the spaces between comments
 
-
-
-
-
-
 #We need a list of words five letters long that our game will select #from.  Declare the list here.
-
-
-
-
-
 
 #Now we will be using a function from the library we imported.  
 #This is the random.choice() function which chooses a word at random #from our list.
 
-
-
-
-
-
 #Next, let’s make a list using the list() function. This will be the #clue that tells our player how many letters are in the word.  We will #use a string of question marks for our clue.
-
-
-
-
-
 
 #Next, select a symbol from https://emojipedia.org/ and declare a variable #with that symbol.    
 
