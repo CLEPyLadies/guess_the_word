fc5759b3e55e3d1cf4405f0295d4b6d3bcd0cf19
mutterberg <Marissa@UtterbergDataDev.com>
2021-08-14 14:12:43 -0400

    Higher level skills

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,49 +1,102 @@
 import random
+from typing import List
 
-guesses_left = 9
 
-words = ['sunny','daisy','water', 'beach','green','music','porch','grill','flower']
+## =============================
+##          CONSTANTS
+## =============================
 
-secret_word = random.choice(words)
+WORD_BANK = [
+    'sunny','daisy','water', 'beach',
+    'green','music','porch','grill','flower'
+    ]
+GUESSES_SYMBOL = "🌞"
 
-clue = list('?????')
+## =============================
+## STRING MANIPULATION FUNCTIONS
+## =============================
 
-symbol = "🌞"
 
-guessed_word_correctly = False
+def update_clue(
+    clue: str,
+    secret_word: str,
+    guessed_letter: str
+    ) -> str:
+    '''Update clue with correctly guessed letters'''
 
-def update_clue(guessed_letter, secret_word):
-    index = 0
-    while index < len(secret_word):
-        if guessed_letter == secret_word[index]:
-            clue[index] = guessed_letter
-        index = index + 1
-    
-def listToString(mylist):
+    # The version we prefer
+    for letter_index, correct_letter in enumerate(secret_word):
+        if guessed_letter == correct_letter:
+            clue[letter_index] = guessed_letter
+
+    # # The original version
+    # index = 0
+    # while index < len(secret_word):
+    #     if guessed_letter == secret_word[index]:
+    #         clue[index] = guessed_letter
+    #     index += 1
+
+    return clue
+
+
+def listToString(mylist: List[str]) -> str:
     cluestring = ' '
     return cluestring.join(mylist)
 
-print("Guess the word associated with Summer! ")
 
-while guesses_left > 0:
-    print(listToString(clue))
-    print("Guesses left: " + symbol * guesses_left)
-    guess = input("Guess a letter or the whole word: ")
-    if guess == secret_word:
-        guessed_word_correctly = True
-        break
-    
-    if guess in secret_word:
-        update_clue(guess, secret_word)
-        
+## =============================
+##     GAME PLAY FUNCTIONS
+## =============================
+
+
+def game_over(word: str, *, winner: bool) -> None:
+    '''End the game'''
+    if winner:
+        print(f"You win!  The secret word was {word}")
     else:
-        print('Incorrect. You lose a life!')
-        guesses_left = guesses_left - 1
+        print(f"You lose! Womp, womp.  The secret word was {word}")
 
-if guessed_word_correctly:
-    print("You win!  The secret word was " + secret_word)
-else:
-    print("You lose! Womp, womp.  The secret word was " + secret_word)
+
+def main() -> None:
+    '''Play the '''
+    guesses_left = 9
+    super_secret_word = random.choice(WORD_BANK)
+    not_so_secret_clue = list('?????')
+    guessed_word_correctly = False
+    print("Guess the word associated with Summer! ")
+
+    while guesses_left > 0:
+
+        # Get a clue!
+        print(listToString(not_so_secret_clue))
+        print(f"Guesses left: {GUESS_SYMBOL * guesses_left}")
+
+        # Make a guess!
+        guess = input("Guess a letter or the whole word: ")
+
+        # Evaluate.
+        if guess == super_secret_word:
+            # We won!
+            game_over(super_secret_word, winner=True)
+            return
+
+        if guess in super_secret_word:
+            # We're getting closer!
+            not_so_secret_clue = update_clue(not_so_secret_clue, super_secret_word, guess)
+
+        else:
+            # Uh oh.
+            print('Incorrect. You lose a life!')
+            guesses_left = guesses_left - 1
+
+    # We're out of guesses. Womp wah.
+    game_over(super_secret_word, winner=False)
+
+
+## =============================
+##       "Dunder main"
+## =============================
+
 
 if __name__ == '__main__':
-    pass
+    main()
