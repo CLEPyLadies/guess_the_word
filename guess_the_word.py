6da03e95e65c2988821d92175bd6a9d5c8a8c3a2
mutterberg <Marissa@UtterbergDataDev.com>
2021-08-14 14:57:59 -0400

    Format code comments in participant script

---
+++ guess_the_word.py
@@ -0,0 +1,89 @@
+# The first step is to import the python libraries that will help us.
+# This is done for you here
+
+import random
+
+# Next we will declare the variable that holds the number of guesses or
+#chances that our player has to correctly guess the word
+# Put your code in the spaces between comments
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# We need a list of words five letters long that our game will select
+# from.  Declare the list here.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Now we will be using a function from the library we imported.  
+# This is the random.choice() function which chooses a word at random
+# from our list.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Next, let’s make a list using the list() function. This will be the
+# clue that tells our player how many letters are in the word.  We will
+# use a string of question marks for our clue.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Next, select a symbol from https://emojipedia.org/ and declare a variable
+# with that symbol.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Now we need to create an indicator that the word has been guessed #correctly.
+# We want the staring value to be False.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Here we will declare the function that updates the clue.
+# ============START CODING==============
+
+
+
+
+
+
+# ============PAUSE CODING==============
+# Here we will write the function that takes the a list and concatenates it into a string with the guessed letters.
+# ============START CODING==============
+
+
+    
+# ============PAUSE CODING==============
+# Now we start building the game play code.
+# Write a print statement that opens the game.
+# ============START CODING==============
+
+# ============PAUSE CODING==============
+# Here, put a while loop that
+#   a) prints the guesses,
+#   b) states how many guesses are left, and
+#   c) allows the person to guess a letter or word.
+# Also, add in an if/else block that updates the clue and the
+# number of guesses left in the game
+# ============START CODING==============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ============PAUSE CODING==============
+# Outside of the loop (aka unindented), set up the ending of the game.
+# Make sure to include both the win condition and the loss condition.
+# ============START CODING==============
+
+
+
+
+# ============PAUSE CODING==============
+# Done! Whether you won or lost, you learned! <3
