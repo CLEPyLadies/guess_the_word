e2373c2e252a1f7c3b865a52f292004c84453179
mutterberg <Marissa@UtterbergDataDev.com>
2021-08-14 15:00:17 -0400

    Outline participant instructions in blank script

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,55 +1,89 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# The first step is to import the python libraries that will help us.
+# This is done for you here
 
-This is a temporary script file.
-"""
 import random
 
-guesses_left = 9
+# Next we will declare the variable that holds the number of guesses or
+# chances that our player has to correctly guess the word
+# Put your code in the spaces between comments
+# ============START CODING==============
 
-words = ['sunny','daisy','water', 'beach','green','music','porch','grill','flower']
+# ============PAUSE CODING==============
+# We need a list of words five letters long that our game will select
+# from.  Declare the list here.
+# ============START CODING==============
 
-secret_word = random.choice(words)
+# ============PAUSE CODING==============
+# Now we will be using a function from the library we imported.  
+# This is the random.choice() function which chooses a word at random
+# from our list.
+# ============START CODING==============
 
-clue = list('?????')
+# ============PAUSE CODING==============
+# Next, let’s make a list using the list() function. This will be the
+# clue that tells our player how many letters are in the word.  We will
+# use a string of question marks for our clue.
+# ============START CODING==============
 
-symbol = "🌞"
+# ============PAUSE CODING==============
+# Next, select a symbol from https://emojipedia.org/ and declare a variable
+# with that symbol.
+# ============START CODING==============
 
-guessed_word_correctly = False
+# ============PAUSE CODING==============
+# Now we need to create an indicator that the word has been guessed #correctly.
+# We want the staring value to be False.
+# ============START CODING==============
 
-def update_clue(guessed_letter, secret_word):
-    index = 0
-    while index < len(secret_word):
-        if guessed_letter == secret_word[index]:
-            clue[index] = guessed_letter
-        index = index + 1
+# ============PAUSE CODING==============
+# Here we will declare the function that updates the clue.
+# ============START CODING==============
+
+
+
+
+
+
+# ============PAUSE CODING==============
+# Here we will write the function that takes the a list and concatenates it into a string with the guessed letters.
+# ============START CODING==============
+
+
     
-def listToString(mylist):
-    cluestring = ' '
-    return cluestring.join(mylist)
+# ============PAUSE CODING==============
+# Now we start building the game play code.
+# Write a print statement that opens the game.
+# ============START CODING==============
 
-print("Guess the word associated with Summer! ")
-
-while guesses_left > 0:
-    print(listToString(clue))
-    print("Guesses left: " + symbol * guesses_left)
-    guess = input("Guess a letter or the whole word: ")
-    if guess == secret_word:
-        guessed_word_correctly = True
-        break
-    
-    if guess in secret_word:
-        update_clue(guess, secret_word)
-        
-    else:
-        print('Incorrect. You lose a life!')
-        guesses_left = guesses_left - 1
-
-if guessed_word_correctly:
-    print("You win!  The secret word was " + secret_word)
-else:
-    print("You lose! Womp, womp.  The secret word was " + secret_word)
+# ============PAUSE CODING==============
+# Here, put a while loop that
+#   a) prints the guesses,
+#   b) states how many guesses are left, and
+#   c) allows the person to guess a letter or word.
+# Also, add in an if/else block that updates the clue and the
+# number of guesses left in the game
+# ============START CODING==============
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# ============PAUSE CODING==============
+# Outside of the loop (aka unindented), set up the ending of the game.
+# Make sure to include both the win condition and the loss condition.
+# ============START CODING==============
+
+
+
+
+# ============PAUSE CODING==============
+# Done! Whether you won or lost, you learned! <3
